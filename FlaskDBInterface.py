074af62c6bb3187f78691c1e986e6f0b0da3b063
cobendaniel <78524895+cobendaniel@users.noreply.github.com>
2024-06-28 14:50:32 +0900

    Update FlaskDBInterface.py

--- FlaskDBInterface.py
+++ FlaskDBInterface.py
@@ -12,28 +12,30 @@ from config.flask_programs.FlaskAPIObject      import marketing_team_two_databas
 @flask_file_server_interface.route("/upload_data", methods = ["POST"])
 @jwt_required()
 def upload_data():
-    table_name   = request.form["table_name"]
-    server_name  = request.form["server_name"]
-    dataframe    = pd.read_json(io.StringIO(request.files["dataframe"].read().decode("utf-8")))
+    table_name  = request.form["table_name"]
+    server_name = request.form["server_name"]
+    schema_name = request.form["schema_name"]
+    dataframe   = pd.read_json(io.StringIO(request.files["dataframe"].read().decode("utf-8")))
     marketing_team_two_database_object.connection_settings("postgresql", os.getenv(ADMIN_NAME), os.getenv(ADMIN_PWD), "localhost", server_name)
 
     try:
-        marketing_team_two_database_object.upload_to_database(table_name, dataframe)
+        marketing_team_two_database_object.upload_to_database(table_name, dataframe, schema_name = schema_name)
         return jsonify({"status" : f"Operation success, data uploaded to {server_name}.{table_name}"})
     except Exception as E:
         return jsonify({"status" : f"Operation failure, error message {E}"})          
-    
+     
 @flask_file_server_interface.route("/query", methods = ["GET"])
 @jwt_required()
 def query_data():
     server_name      = request.args.get("server_name")
     table_name       = request.args.get("table_name")
+    schema_name      = request.args.get("schema_name")
     columns_list     = request.args.get("column_names").split(",")
     filter_condition = request.args.get("filter_value")
 
     try:
         marketing_team_two_database_object.connection_settings("postgresql", os.getenv(ADMIN_NAME), os.getenv(ADMIN_PWD), "localhost", server_name)
-        result_json_data = pd.DataFrame(marketing_team_two_database_object.get_from_database(table_name, columns_list, filter_condition))
+        result_json_data = pd.DataFrame(marketing_team_two_database_object.get_from_database(table_name, columns_list, filter_condition, schema_name = schema_name))
         return result_json_data.to_json()
     except Exception as E:
         return jsonify({"status" : f"Operation failure, error message {E}"})
@@ -43,13 +45,14 @@ def query_data():
 def delete_data():
     table_id         = request.args.get("table_id")
     server_name      = request.args.get("server_name")
+    schema_name      = request.args.get("schema_name")
     column_name      = request.args.get("column_name")
     filter_value     = request.args.get("filter_value")
     filter_condition = request.args.get("filter_condition")
 
     try:
         marketing_team_two_database_object.connection_settings("postgresql", os.getenv(ADMIN_NAME), os.getenv(ADMIN_PWD), "localhost", server_name)
-        marketing_team_two_database_object.delete_from_database(table_id, column_name, filter_value, filter_condition)
+        marketing_team_two_database_object.delete_from_database(table_id, column_name, filter_value, filter_condition, schema_name = schema_name)
         return jsonify({"status" : "success"})
     except Exception as E:
         return jsonify({"status" : f"Operation failure, error message {E}"})
