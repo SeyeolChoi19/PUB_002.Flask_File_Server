46ed8c4b7987bd5bc05a063e1ea1e87099540687
cobendaniel <78524895+cobendaniel@users.noreply.github.com>
2024-06-28 14:53:07 +0900

    Create EnvironmentVariables.py

---
+++ config/miscellaneous/EnvironmentVariables.py
@@ -0,0 +1,7 @@
+INFO_KEY             = "INFO_KEY" # FERNET_KEY
+ADMIN_NAME           = "ADMIN_NAME" # 관리자 계정
+ADMIN_PWD            = "ADMIN_PWD" # 관리자 비번
+TABLE_NAME           = "TABLE_NAME" # 유저 정보 테이블 
+SERVER_NAME          = "SERVER_NAME" # 서버명 
+DB_INFO_TABLE        = "DB_INFO_TABLE" # DB 유저 정보 테이블 
+ALLOWED_IP_ADDRESSES = "ALLOWED_IP_ADDRESSES" # 허용되는 IP 주소
