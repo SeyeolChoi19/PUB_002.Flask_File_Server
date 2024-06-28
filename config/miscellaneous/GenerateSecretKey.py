3d3c420713054170aef3fe1a576c502b67363588
cobendaniel <78524895+cobendaniel@users.noreply.github.com>
2024-06-28 14:51:45 +0900

    Create GenerateSecretKey.py

---
+++ config/miscellaneous/GenerateSecretKey.py
@@ -0,0 +1,12 @@
+import string, random 
+
+def generate_secret_key(key_length: int = 30):
+    key_components = list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(range(0, 10)) + [character for character in list(string.punctuation) if (character not in ["(", ")", "[", "]", "\\"])]
+    output_string  = ""
+
+    for digit in range(key_length):
+        output_string += str(random.choice(key_components))
+
+    return output_string 
+
+SECRET_KEY = generate_secret_key()
