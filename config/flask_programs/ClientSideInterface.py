42672a4b1d6238891dd28d3c9e3b9673897f51a8
SeyeolChoi19 <78524895+SeyeolChoi19@users.noreply.github.com>
2024-08-19 17:44:34 +0900

    Update ClientSideInterface.py

--- config/flask_programs/ClientSideInterface.py
+++ config/flask_programs/ClientSideInterface.py
@@ -1,24 +1,9 @@
-"""
-1. `ClientSideInterface` 클래스의 인스턴스 생성
-2. `login_to_server` 메서드를 사용하여 서버에 로그인
-3. 데이터를 데이터베이스에 업로드하려면 `upload_to_database` 
-4. 데이터베이스에서 데이터를 쿼리하려면 `query_database` 
-"""
 import os, base64, requests
 
 import pandas as pd 
 
-from config.DBInterfacePostgres import DBInterface
-
 class ClientSideInterface:
     def login_to_server(self, username: str, password: str, target_ip: str):
-        """
-        서버에 로그인
-            - username   : 사용자 이름
-            - password   : 비밀번호
-            - target_ip  : 타겟 서버의 IP 주소
-            - 반환값      : 로그인 성공 여부 메시지
-        """
         api_url  = f"{target_ip}/login"
         response = requests.post(
             api_url, 
@@ -34,16 +19,92 @@ class ClientSideInterface:
         else:
             return "login failed"
 
+    def upload_file_to_server(self, local_file_name: str, server_file_name: str, target_ip: str):
+        server_directory_array = server_file_name.replace("\\", "/").split("/")
+        server_directory       = "/".join(server_directory_array[:-1])
+        file_name              = server_directory_array[-1]
+        
+        with open(local_file_name, "rb") as file_object:
+            file_object_base64 = base64.b64encode(file_object.read()).decode()
+
+        api_url  = f"{target_ip}/save_file"
+        response = requests.post(
+            api_url, 
+            headers = {
+                "Authorization" : f"Bearer {self.jwt_token}"
+            },
+            data = {
+                "file_name"        : file_name, 
+                "server_directory" : server_directory
+            },
+            files = {
+                "transferred_file" : file_object_base64
+            }
+        )
+
+        return response.json()["status"]
+
+    def retrieve_file_from_server(self, server_file_name: str, destination_folder: str, target_ip: str):
+        server_directory_array = server_file_name.replace("\\", "/").split("/")
+        server_directory       = "/".join(server_directory_array[:-1])
+        file_name              = server_directory_array[-1]
+
+        api_url  = f"{target_ip}/retrieve_file"
+        response = requests.get(
+            api_url, 
+            headers = {
+                "Authorization" : f"Bearer {self.jwt_token}"
+            },
+            params = {
+                "file_name"      : file_name, 
+                "file_directory" : server_directory
+            }
+        )
+
+        with open(os.path.join(destination_folder, file_name), "wb") as retrieved_file: 
+            retrieved_file.write(base64.b64decode(response.json()["file_object"]))
+
+        return response
+
+    def search_file_server(self, server_file_name: str, target_ip: str):
+        server_directory_array = server_file_name.replace("\\", "/").split("/")
+        file_directory         = "/".join(server_directory_array[:-1])
+        file_name              = server_directory_array[-1]
+
+        api_url  = f"{target_ip}/search_file"
+        response = requests.get(
+            api_url, 
+            headers = {
+                "Authorization" : f"Bearer {self.jwt_token}"
+            },
+            params = {
+                "file_name"      : file_name, 
+                "file_directory" : file_directory
+            }
+        )
+
+        return response.json()["status"]
+
+    def delete_from_server(self, server_file_name: str, target_ip: str):
+        server_directory_array = server_file_name.replace("\\", "/").split("/")
+        file_directory         = "/".join(server_directory_array[:-1])
+        file_name              = server_directory_array[-1]
+
+        api_url  = f"{target_ip}/delete_file"
+        response = requests.delete(
+            api_url, 
+            headers = {
+                "Authorization" : f"Bearer {self.jwt_token}"
+            },
+            params = {
+                "file_name"      : file_name, 
+                "file_directory" : file_directory 
+            }
+        )
+
+        return response.json()
+    
     def upload_to_database(self, table_name: str, server_name: str, target_ip: str, data_to_upload: pd.DataFrame, schema_name: str = "public"):
-        """
-        데이터를 데이터베이스에 업로드하는 함수.
-            - table_name: 테이블 이름
-            - server_name: 서버 이름
-            - target_ip: 타겟 서버의 IP 주소
-            - data_to_upload: 업로드할 데이터프레임
-            - schema_name: 스키마 이름 (기본값: "public")
-            - 반환값: 업로드 상태 메시지
-        """
         api_url  = f"{target_ip}/upload_data"
         response = requests.post(
             api_url, 
@@ -63,18 +124,8 @@ class ClientSideInterface:
         return response
     
     def query_database(self, table_name: str, server_name: str, target_ip: str,  columns_list: list[str], filter_value: str = None, schema_name: str = "public"):
-        """
-        데이터베이스에서 데이터를 쿼리함.
-            - table_name: 테이블 이름
-            - server_name: 서버 이름
-            - target_ip: 타겟 서버의 IP 주소
-            - columns_list: 조회할 컬럼 리스트
-            - filter_value: 필터 조건 (옵션)
-            - schema_name: 스키마 이름 (기본값: "public")
-            - 반환값: 쿼리 결과
+        filter_value = "" if (filter_value == None) else filter_value
 
-        메서드에서 반환된 "response" 객체의 .json()을 pd.DataFrame()으로 감싸면 데이터프레임으로 변환됨 -> 하단 if-else 문 참조
-        """
         api_url  = f"{target_ip}/query"
         response = requests.get(
             api_url, 
@@ -87,6 +138,25 @@ class ClientSideInterface:
                 "schema_name"      : schema_name,
                 "column_names"     : ",".join(columns_list),
                 "filter_condition" : (lambda x: "" if (x == None) else x)(filter_value)
+            }
+        )
+        
+        return response
+    
+    def delete_from_database(self, table_name: str, server_name: str, target_ip: str, column_name: str, filter_value: str, filter_condition: str, schema_name: str = "public"):
+        api_url  = f"{target_ip}/delete"
+        response = requests.delete(
+            api_url, 
+            headers = {
+                "Authorization" : f"Bearer {self.jwt_token}"
+            },
+            params = {
+                "table_id"         : table_name,
+                "server_name"      : server_name,
+                "schema_name"      : schema_name,
+                "column_name"      : column_name, 
+                "filter_value"     : filter_value,
+                "filter_condition" : filter_condition
             }
         )
         
